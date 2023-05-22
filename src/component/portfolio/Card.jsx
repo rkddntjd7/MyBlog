8be27d42460317c1e2aabae9d8df4511fdcb7d9c
rkddntjd7 <125332196+rkddntjd7@users.noreply.github.com>
2023-05-22 16:26:09 +0900

    last work

--- src/component/portfolio/Card.jsx
+++ src/component/portfolio/Card.jsx
@@ -2,18 +2,21 @@ import React, { useState } from 'react';
 import { FcLike, FcRight } from "react-icons/fc";
 import { FaRegThumbsUp, FaAngleRight, FaTimes, } from "react-icons/fa";
 
-const Card = ({img, title, category, totalLike}) => {
+const Card = ({index, img, title, category, totalLike}) => {
     const [modal, setModal] = useState(false);
+    const toggleModal = () => {
+        setModal(!modal);
+    }
 
     return (
         <>
         <div className="box btn-shadow card">
             <div className="img">
-                <img src={img} alt={img} />
+                <img src={img} alt={img} onClick={toggleModal}/>
             </div>
             <div className="category d-flex">
                 <span>
-                    {(category)}
+                    {category}
                 </span>
                 <label>
                     <FcLike />
@@ -22,16 +25,16 @@ const Card = ({img, title, category, totalLike}) => {
             </div>
             <div className="title">
                 <h2>{title}</h2>
-                <a href="#popup" className='arrow'>
+                <a href="#popup" className='arrow' onClick={toggleModal}>
                     <FcRight />
                 </a>
             </div>
         </div>
         {modal && (
-        <div className="modal">
+        <div className="modal overlay">
             <div className="modal-content d-flex">
                 <div className="modal-img left">
-                    <img src={img} alt={img} />
+                    <img src={img} alt={img} onClick={toggleModal} />
                 </div>
                 <div className="modal-text right">
                     <span>Feadtures - Design</span>
@@ -45,7 +48,7 @@ const Card = ({img, title, category, totalLike}) => {
                     <button className="btn-shadow">
                         VIEW PROJECT <FaAngleRight />
                     </button>
-                    <button className="btn-shadow">
+                    <button className="btn-shadow close-modal" onClick={toggleModal}>
                         <FaTimes />
                     </button>
                 </div>
